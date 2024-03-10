79c6a1405c4f920eb4a0ef1c23a73f1f15b75e7c
Shubham Lal <im.shubhamlal@gmail.com>
2024-03-10 14:32:04 +0530

    wip: add profile in right sidebar & nav

--- client/src/components/sidebar/right-sidebar.tsx
+++ client/src/components/sidebar/right-sidebar.tsx
@@ -1,6 +1,34 @@
+import "./right-sidebar.css";
+import { RxHamburgerMenu } from "react-icons/rx";
+
+
 const RightSidebar = () => {
   return (
-    <div id="right-sidebar">Right</div>
+    <div id='right-sidebar'>
+      <div className='logo__container'>
+        <img src="/logo.png" alt="Logo" />
+      </div>
+
+      <div style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
+        <div className='profile-theme__container'>
+          <div className='profile__wrapper'>
+            <img src="/user.jpg" alt="User" className='profile__image' />
+            <div className='profile__info'>
+              <p>Julie Roberts</p>
+              <p>julieroberts</p>
+            </div>
+          </div>
+
+          <div className='theme__wrapper'>
+            <div className='theme__button'>
+
+            </div>
+          </div>
+        </div>
+
+        <RxHamburgerMenu className='menu-icon' />
+      </div>
+    </div>
   )
 }
 
