31759da026aa007146d187ccc9c91a5463b97f28
Shubham Lal <im.shubhamlal@gmail.com>
2024-03-10 12:45:18 +0530

    wip: add left sidebar links

--- client/src/components/sidebar/left-sidebar.tsx
+++ client/src/components/sidebar/left-sidebar.tsx
@@ -1,6 +1,22 @@
+import "./left-sidebar.css";
+import { leftSidebarLinks } from "../../data/left-sidebar-links";
+import { Link } from "react-router-dom";
+
+
 const LeftSidebar = () => {
   return (
-    <div id="left-sidebar">Left</div>
+    <div id='left-sidebar'>
+      <ul>
+        {leftSidebarLinks.map(item => (
+          <li key={item.id}>
+            <Link to={item.href} className='links__wrapper'>
+              <item.icon />
+              <p>{item.name}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
---
+++ client/src/data/left-sidebar-links.ts
@@ -0,0 +1,38 @@
+import { RiHome2Fill } from "react-icons/ri";
+import { RiFireFill } from "react-icons/ri";
+import { FaFeather } from "react-icons/fa6";
+import { IoSearch } from "react-icons/io5";
+import { PiBellSimpleFill } from "react-icons/pi";
+
+export const leftSidebarLinks = [
+    {
+        id: 1,
+        name: "Home",
+        href: "/",
+        icon: RiHome2Fill
+    },
+    {
+        id: 2,
+        name: "Hot Topics",
+        href: "/hot-topics",
+        icon: RiFireFill
+    },
+    {
+        id: 3,
+        name: "Open Topics",
+        href: "/open-topics",
+        icon: FaFeather
+    },
+    {
+        id: 4,
+        name: "Search",
+        href: "/search",
+        icon: IoSearch
+    },
+    {
+        id: 5,
+        name: "Notifications",
+        href: "/notifications",
+        icon: PiBellSimpleFill
+    }
+]
