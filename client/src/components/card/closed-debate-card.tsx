32fd6103e1f985aa796040851f5bd4ba37bcc9f4
Shubham Lal <im.shubhamlal@gmail.com>
2024-03-14 17:59:22 +0530

    wip: designed card

--- client/src/components/card/closed-debate-card.tsx
+++ client/src/components/card/closed-debate-card.tsx
@@ -9,12 +9,22 @@ const ClosedDebateCard: React.FC<CardProps> = () => {
     return (
         <div id='closed-card'>
             <div className='left'>
-                <h2>Sony is the best camera. Sony is the best camera.</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero suscipit eos, nemo unde labore deleniti minima laboriosam harum.</p>
+                <h2>Sony is the best camera in the current segment in the current scenario</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Los nemo unde labore deleniti minima laboriosam harum.</p>
+                <button>View</button>
             </div>
             <div className='divider' />
             <div className='right'>
-                Down/Right
+                <div className='user-info'>
+                    <div className='debate-from'>
+                        <img src="/user1.webp" alt="" />
+                        <p>Aniket Das</p>
+                    </div>
+                    <div className='debate-by'>
+                        <img src="/user2.jpeg" alt="" />
+                        <p>Pratik Prasad</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
