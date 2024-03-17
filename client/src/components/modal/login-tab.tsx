5ea1a278789f89d874fe990d85aff3d1aa3ca8e0
Shubham Lal <im.shubhamlal@gmail.com>
2024-03-17 15:53:30 +0530

    add Login tab

--- client/src/components/modal/login-tab.tsx
+++ client/src/components/modal/login-tab.tsx
@@ -1,7 +1,39 @@
+import "./login-tab.css";
+import { FcGoogle } from "react-icons/fc";
+
 const LoginTab = () => {
+    const handleLoginSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    }
+
     return (
         <div id='login'>
-
+            <div className='login__header'>
+                <h3>Login</h3>
+                <p>New here? <span>Create Account</span></p>
+            </div>
+            <form className='form__container' onSubmit={handleLoginSubmit}>
+                <div className='input__container'>
+                    <p>Email/Username</p>
+                    <input />
+                </div>
+                <div className='input__container'>
+                    <p>Password</p>
+                    <input />
+                </div>
+                <button type='submit'>
+                    Log in
+                </button>
+            </form>
+            <div className='or-divider'>
+                <div className='divider' />
+                <p>or</p>
+                <div className='divider' />
+            </div>
+            <button className='google-btn'>
+                <FcGoogle size={25} />
+                <span>Continue with Google</span>
+            </button>
         </div>
     )
 }
--- client/src/components/sidebar/right-sidebar.tsx
+++ client/src/components/sidebar/right-sidebar.tsx
@@ -2,61 +2,43 @@ import "./right-sidebar.css";
 import { Link, useLocation } from "react-router-dom";
 import { IoSearch } from "react-icons/io5";
 import ToggleTheme from "../button/toggle-theme";
-// import ExpandMenu from "../menu/expand-menu";
 import Profile from "./profile";
 import Explore from "./explore";
 import { useNavStore } from "../../store/useNavStore";
 
 const RightSidebar = () => {
-  return (
-    <div id='right-sidebar'>
-      {/* <div className={`expand-menu__background ${expand ? 'expand' : ''}`} /> */}
-
-      <SidebarContent />
-
-      {/* {expand && <ExpandMenu setExpand={setExpand} />} */}
-    </div>
-  )
-}
-
-const SidebarContent = () => {
   const location = useLocation();
   const { expand, setExpand } = useNavStore();
 
   return (
-    <div className='right-sidebar__container'>
-      <Link to='/' className='logo__container'>
-        <img src="/logo.png" />
-      </Link>
+    <div id='right-sidebar'>
+      <div className='right-sidebar__container'>
+        <Link to='/' className='logo__container'>
+          <img src="/logo.png" />
+        </Link>
 
-      <div style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
-        <div className='search-button' style={{ borderColor: `${location.pathname === "/search" ? "var(--body_color)" : ""}` }}>
-          <Link to='/search' onClick={() => setExpand(false)}>
-            <IoSearch size={15} />
-          </Link>
-        </div>
-
-        <div className='profile-theme__container'>
-          {/* <div className={`theme__wrapper ${expand && 'expand-active'}`}> */}
-          <div className={`theme__wrapper`}>
-            <ToggleTheme />
+        <div style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
+          <div className='search-button' style={{ borderColor: `${location.pathname === "/search" ? "var(--body_color)" : ""}` }}>
+            <Link to='/search' onClick={() => setExpand(false)}>
+              <IoSearch size={15} />
+            </Link>
           </div>
 
-          <Profile />
+          <div className='profile-theme__container'>
+            <div className={`theme__wrapper`}>
+              <ToggleTheme />
+            </div>
+
+            <Profile />
+          </div>
         </div>
 
-        {/* <div className='menu-icon' onClick={() => setExpand(!expand)}>
-          <div className={`hamburger__line ${expand ? 'animate' : ''}`} />
-          <div className={`hamburger__line ${expand ? 'animate' : ''}`} />
-          <div className={`hamburger__line ${expand ? 'animate' : ''}`} />
-        </div> */}
-      </div>
+        <div className='explore__container'>
+          <Explore />
+        </div>
 
-      <div className='explore__container'>
-        <Explore />
+        {!expand && <div className='nav-border' />}
       </div>
-
-      {!expand && <div className='nav-border' />}
     </div>
   )
 }
