a3a9171885756ee6470356d7d967a0c388f90f6d
Shubham-Lal <im.shubhamlal@gmail.com>
2024-07-09 23:21:37 +0530

    client: add bounds to editor

--- client/src/pages/create-debate/index.tsx
+++ client/src/pages/create-debate/index.tsx
@@ -1,4 +1,4 @@
-import "./styles.css"
+import "./style.css"
 import { useState } from "react"
 import ReactQuill from "react-quill"
 import "react-quill/dist/quill.snow.css"
@@ -8,40 +8,42 @@ export default function CreateDebatePage() {
     const { sidebar } = useNavStore();
 
     const [value, setValue] = useState('');
-
     console.log(value);
 
     return (
-        <ReactQuill
-            className={sidebar ? '' : 'h-full'}
-            theme="snow"
-            value={value}
-            onChange={setValue}
-            modules={{
-                toolbar: [
-                    ['bold', 'italic', 'underline', 'strike'],
-                    ['blockquote', 'code-block'],
-                    ['link', 'formula'],
+        <div id="create">
+            <ReactQuill
+                className={sidebar ? '' : 'h-full'}
+                theme="snow"
+                value={value}
+                onChange={setValue}
+                modules={{
+                    toolbar: [
+                        ['bold', 'italic', 'underline', 'strike'],
+                        ['blockquote', 'code-block'],
+                        ['link', 'formula'],
 
-                    [{ 'header': 1 }, { 'header': 2 }],
-                    [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'list': 'check' }],
-                    [{ 'script': 'sub' }, { 'script': 'super' }],
-                    [{ 'indent': '-1' }, { 'indent': '+1' }],
-                    [{ 'direction': 'rtl' }],
+                        [{ 'header': 1 }, { 'header': 2 }],
+                        [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'list': 'check' }],
+                        [{ 'script': 'sub' }, { 'script': 'super' }],
+                        [{ 'indent': '-1' }, { 'indent': '+1' }],
+                        [{ 'direction': 'rtl' }],
 
-                    [{ 'size': ['small', false, 'large', 'huge'] }],
-                    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+                        [{ 'size': ['small', false, 'large', 'huge'] }],
+                        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
 
-                    [{ 'color': [] }, { 'background': [] }],
-                    [{ 'font': [] }],
-                    [{ 'align': [] }],
+                        [{ 'color': [] }, { 'background': [] }],
+                        [{ 'font': [] }],
+                        [{ 'align': [] }],
 
-                    ['clean']
-                ],
-                clipboard: {
-                    matchVisual: false,
-                }
-            }}
-        />
+                        ['clean']
+                    ],
+                    clipboard: {
+                        matchVisual: false,
+                    }
+                }}
+                bounds={`#create`}
+            />
+        </div >
     );
 }
