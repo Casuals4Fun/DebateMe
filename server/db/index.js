140773640451acae03db9694c50bec1491246671
Shubham-Lal <im.shubhamlal@gmail.com>
2024-08-19 13:49:23 +0530

    server: add port to mysql

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,11 @@
 const { createPool } = require('mysql2/promise')
+const { readFileSync } = require('fs')
 
 const pool = createPool({
-    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     database: process.env.DB_DATABASE,
     waitForConnections: true,
     connectionLimit: 10,
